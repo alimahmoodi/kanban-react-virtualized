4c849644b925b2df9d08584b67a67172e7dd2d02
mahmoodi <alimahmudi.alami@gmail.com>
2021-10-01 02:07:39 +0330

    multi column and catch bug fix

--- src/App.js
+++ src/App.js
@@ -10,34 +10,60 @@ import {
 } from "react-virtualized";
 import faker from "faker";
 
+let uniqueId = 0;
+function getItems(count) {
+  return Array.from({ length: count }, (_v, _k) => {
+    const id = uniqueId++;
+    return {
+      id: `${id}test`,
+      text: `item ${id}`,
+    };
+  });
+}
+
+const columnsFromBackend = {
+  "column-0": {
+    id: "column-0",
+    title: "در کارتابل",
+    items: getItems(50),
+  },
+  "column-1": {
+    id: "column-1",
+    title: "در دست قدام",
+    items: getItems(50),
+  },
+  "column-2": {
+    id: "column-2",
+    title: "خاتمه یافته",
+    items: getItems(50),
+  },
+  "column-3": {
+    id: "column-3",
+    title: "خاتمه یافته2",
+    items: getItems(50),
+  },
+};
+
+const cache = new CellMeasurerCache({
+  fixedWidth: true,
+  defaultHeight: 100,
+});
+
 export default function App() {
-  const cache = React.useRef(
-    new CellMeasurerCache({
-      fixedWidth: true,
-      defaultHeight: 100,
-    })
-  );
-  const listRef = useRef();
+  // const cache = React.useRef(
+  //   new CellMeasurerCache({
+  //     fixedWidth: true,
+  //     defaultHeight: 100,
+  //   })
+  // );
 
   const [people, setPeople] = React.useState([]);
 
-  React.useEffect(() => {
-    setPeople(
-      [...Array(3000).keys()].map((key) => {
-        return {
-          id: `${key}test`,
-          name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-          bio: faker.lorem.lines(Math.random() * 10),
-        };
-      })
-    );
-  }, []);
+  const [columns, setColumns] = useState(columnsFromBackend);
 
-  const handleHover = (index) => {
-    // cache.current.clear(index);
-    // listRef.current.forceUpdateGrid();
-    // setClickRow(index);
-  };
+  React.useEffect(() => {
+    setColumns(columnsFromBackend);
+  }, []);
 
   const onDragEnd = (result) => {
     if (!result.destination) return;
@@ -58,104 +84,128 @@ export default function App() {
     };
   }
 
-  const rowRender = ({ key, index, style, parent }) => {
-    const person = people[index];
-    const patchedStyle = {
-      ...style,
-      // left: style.left + 8,
-      // top: style.top + 8,
-      // width: `calc(${style.width} - ${8 * 2}px)`,
-      // height: style.height - 8,
-    };
-    return (
-      <CellMeasurer
-        key={person.id}
-        cache={cache.current}
-        parent={parent}
-        columnIndex={0}
-        rowIndex={index}
-      >
-        {({ registerChild }) => (
-          <Draggable draggableId={person.id} index={index}>
-            {(provided) => (
-              <div
-                ref={provided.innerRef}
-                {...provided.draggableProps}
-                {...provided.dragHandleProps}
-                style={getStyle(provided, patchedStyle)}
-              >
-                <div style={{ border: "1px solid black", margin: 8 }}>
-                  <h2>{person.name}</h2>
-                  <p>{person.bio}</p>
+  const rowRender =
+    (items) =>
+    ({ key, index, style, parent }) => {
+      const item = items[index];
+      console.log(item, "item");
+      const patchedStyle = {
+        ...style,
+        // left: style.left + 8,
+        // top: style.top + 8,
+        // width: `calc(${style.width} - ${8 * 2}px)`,
+        // height: style.height - 8,
+      };
+      return (
+        <CellMeasurer
+          key={item.id}
+          cache={cache}
+          parent={parent}
+          columnIndex={0}
+          rowIndex={index}
+        >
+          {({ registerChild }) => (
+            <Draggable draggableId={item.id} index={index}>
+              {(provided) => (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  style={getStyle(provided, patchedStyle)}
+                >
+                  <div style={{ border: "1px solid black", margin: 8 }}>
+                    <h2>{item.text}</h2>
+                    {/*<p>{person.bio}</p>*/}
+                  </div>
                 </div>
-              </div>
-            )}
-          </Draggable>
-        )}
-      </CellMeasurer>
-    );
-  };
+              )}
+            </Draggable>
+          )}
+        </CellMeasurer>
+      );
+    };
 
   return (
     <div>
       <div
         style={{
+          display: "flex",
           // width: "90%",
           height: "80vh",
           border: "1px solid black",
         }}
       >
         <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable
-            renderClone={(provided, snapshot, rubric) => (
+          {Object.entries(columns).map(([columnId, column], index) => {
+            return (
               <div
-                ref={provided.innerRef}
-                {...provided.draggableProps}
-                {...provided.dragHandleProps}
-                // style={{ margin: 0 }}
+                style={{
+                  display: "flex",
+                  flex: 1,
+                  flexDirection: "column",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+                key={columnId}
               >
-                <div style={{ border: "1px solid black" }}>
-                  <h2>{people[rubric.source.index].name}</h2>
-                  <p>{people[rubric.source.index].bio}</p>
-                </div>
-              </div>
-            )}
-            mode={"virtual"}
-            droppableId="list"
-          >
-            {(provided) => (
-              <div
-                style={{ width: "100%", height: "100%" }}
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-              >
-                <AutoSizer>
-                  {({ width, height }) => (
-                    <List
-                      ref={(ref) => {
-                        // react-virtualized has no way to get the list's ref that I can so
-                        // So we use the `ReactDOM.findDOMNode(ref)` escape hatch to get the ref
-                        if (ref) {
-                          // eslint-disable-next-line react/no-find-dom-node
-                          const whatHasMyLifeComeTo = ReactDOM.findDOMNode(ref);
-                          if (whatHasMyLifeComeTo instanceof HTMLElement) {
-                            provided.innerRef(whatHasMyLifeComeTo);
-                          }
-                        }
-                      }}
-                      width={width}
-                      height={height}
-                      rowHeight={cache.current.rowHeight}
-                      deferredMeasurementCache={cache.current}
-                      rowCount={people.length}
-                      rowRenderer={rowRender}
-                    />
+                <h2>{column.title}</h2>
+
+                <Droppable
+                  renderClone={(provided, snapshot, rubric) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      // style={{ margin: 0 }}
+                    >
+                      <div style={{ border: "1px solid black" }}>
+                        <h2>{column.items[rubric.source.index].text}</h2>
+                        {/*<p>{people[rubric.source.index].bio}</p>*/}
+                      </div>
+                    </div>
                   )}
-                </AutoSizer>
-                {/*{provided.placeholder}*/}
+                  mode={"virtual"}
+                  droppableId={columnId}
+                >
+                  {(provided) => (
+                    <div
+                      style={{ width: "100%", height: "100%" }}
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                    >
+                      <AutoSizer>
+                        {({ width, height }) => (
+                          <List
+                            ref={(ref) => {
+                              // react-virtualized has no way to get the list's ref that I can so
+                              // So we use the `ReactDOM.findDOMNode(ref)` escape hatch to get the ref
+                              if (ref) {
+                                // eslint-disable-next-line react/no-find-dom-node
+                                const whatHasMyLifeComeTo =
+                                  ReactDOM.findDOMNode(ref);
+                                if (
+                                  whatHasMyLifeComeTo instanceof HTMLElement
+                                ) {
+                                  provided.innerRef(whatHasMyLifeComeTo);
+                                }
+                              }
+                            }}
+                            width={width}
+                            height={height}
+                            rowHeight={cache.rowHeight}
+                            deferredMeasurementCache={cache.current}
+                            rowCount={column.items.length}
+                            rowRenderer={rowRender(column.items)}
+                          />
+                        )}
+                      </AutoSizer>
+                      {/*{provided.placeholder}*/}
+                    </div>
+                  )}
+                </Droppable>
               </div>
-            )}
-          </Droppable>
+            );
+          })}
         </DragDropContext>
       </div>
     </div>
